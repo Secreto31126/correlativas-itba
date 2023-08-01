7604799520f7356e64dc99ca8bb77259a96e8954
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2023-07-31 21:14:27 -0300

    Improved landing page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,16 +11,4 @@
 		font-family: 'Roboto', sans-serif;
 		text-align: center;
 	}
-
-	:global(h1) {
-		font-size: max(calc(200px - 10vw), 36px);
-	}
-
-	:global(a) {
-		font-size: max(calc(150px - 10vw), 21px);
-	}
-
-	:global(p) {
-		font-size: 1.5rem;
-	}
 </style>
