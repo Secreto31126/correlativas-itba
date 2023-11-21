ad9ede43c89345a9e8b0bdfec989612a8a53174a
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2023-11-21 00:47:14 -0300

    Soo much cleaner
    I hated the previous implementation

--- src/routes/[materia]/+page.server.ts
+++ src/routes/[materia]/+page.server.ts
@@ -3,16 +3,16 @@ import type { Subject, FilledSubject } from '$lib/types/subjects';
 
 import { error } from '@sveltejs/kit';
 import { codify } from '$lib/modules/codes';
-import { isPath } from '$lib/server/modules/files';
+import Careers from '$lib/server/data/index.json';
 
 export const load = (async ({ params }) => {
-	const files = import.meta.glob('$lib/server/data/*.json', { as: 'raw' });
-	const path = Object.keys(files).find((p) => isPath(params.materia, p));
+	const filename = Careers.find((c) => params.materia === c.cute)?.file;
+	if (!filename) throw error(404, `Materia ${params.materia} not found`);
 
-	if (!path) throw error(404, `Materia ${params.materia} not found`);
-
-	let data;
+	let data: Subject[];
 	try {
+		const files = import.meta.glob('$lib/server/data/*.json', { as: 'raw' });
+		const path = Object.keys(files).find((e) => e.endsWith(filename)) ?? 'LOL no.';
 		data = JSON.parse(await files[path]()) as Subject[];
 	} catch (e) {
 		throw error(500, 'Failed to open file');
