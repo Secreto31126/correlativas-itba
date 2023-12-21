6b48c210dd911e4a0ccaab1bff6147b4d4879af6
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2023-12-21 14:49:37 -0300

    Ups

--- src/lib/components/GoogleButton.svelte
+++ src/lib/components/GoogleButton.svelte
@@ -19,7 +19,7 @@
 		{/if}
 	</a>
 {:else}
-	<button on:click={() => signInWith('google')}>
+	<button on:click={() => signInWith('google')} class="h-full">
 		<img
 			src="https://lh3.googleusercontent.com/COxitqgJr1sJnIDe8-jiKhxDx1FrYbtRHKJ9z_hELisAlapwE9LUPh6fcXIfb5vwpbMl4xl9H9TRFPc5NOO8Sb3VSgIBrfRYvW6cUA"
 			alt="Login"
