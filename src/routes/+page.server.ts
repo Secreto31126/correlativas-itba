7695f973d11993ea68f43b5a27c893e4fa326abe
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2023-12-21 11:41:41 -0300

    Get UserData on landing page

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,25 @@
 import type { PageServerLoad } from './$types';
-import Careers from '$lib/server/data/index.json';
+import type { UserData } from '$lib/types/documents';
 
-export const load = (async () => {
-	return { careers: Careers };
+import Careers from '$lib/server/data/index.json';
+import { createDocument, getDocuments } from '$lib/server/modules/firebase';
+
+export const load = (async ({ locals }) => {
+	let user_data: UserData | null = null;
+	if (locals.userSession) {
+		const { uid } = locals.userSession;
+		const docs = <UserData[]>await getDocuments('user_data', uid);
+
+		if (!docs.length) {
+			const doc = <UserData>await createDocument('user_data', uid);
+			user_data = doc;
+		} else {
+			user_data = docs[0];
+		}
+	}
+
+	return {
+		user_data,
+		careers: Careers
+	};
 }) satisfies PageServerLoad;
