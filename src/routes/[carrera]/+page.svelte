8d3e1862cd291c9bcc7c09461ffc711de6385e04
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2025-01-21 18:49:40 -0300

    MVP for scheduler link

--- src/routes/[carrera]/+page.svelte
+++ src/routes/[carrera]/+page.svelte
@@ -166,6 +166,12 @@
 		counter_type = counter_type === 'credits' ? 'subjects' : 'credits';
 	}
 
+	const scheduler_link = $derived(
+		`https://ceitba.org.ar/scheduler/${data.career_data.itba}?plan=${data.career_data.plan}&code=${selected
+			.map((code) => data.career.find((s) => s.codec === code)?.code)
+			.join('&code=')}`
+	);
+
 	onMount(async () => {
 		interact('.cuatrimestre > div').draggable({
 			inertia: true,
@@ -249,6 +255,31 @@
 			{/if}
 			{#if selected.length}
 				<div class="flex md:pr-2 [&>*]:w-5 md:[&>*]:w-6 gap-4">
+					<!-- https://heroicons.com/solid -->
+					{#if data.career_data.itba}
+						<a
+							href={scheduler_link}
+							title="Ir al planificador"
+							aria-label="Scheduler"
+							class="flex items-center"
+						>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 24 24"
+								fill="currentColor"
+								class="size-6"
+							>
+								<path
+									d="M12 11.993a.75.75 0 0 0-.75.75v.006c0 .414.336.75.75.75h.006a.75.75 0 0 0 .75-.75v-.006a.75.75 0 0 0-.75-.75H12ZM12 16.494a.75.75 0 0 0-.75.75v.005c0 .414.335.75.75.75h.005a.75.75 0 0 0 .75-.75v-.005a.75.75 0 0 0-.75-.75H12ZM8.999 17.244a.75.75 0 0 1 .75-.75h.006a.75.75 0 0 1 .75.75v.006a.75.75 0 0 1-.75.75h-.006a.75.75 0 0 1-.75-.75v-.006ZM7.499 16.494a.75.75 0 0 0-.75.75v.005c0 .414.336.75.75.75h.005a.75.75 0 0 0 .75-.75v-.005a.75.75 0 0 0-.75-.75H7.5ZM13.499 14.997a.75.75 0 0 1 .75-.75h.006a.75.75 0 0 1 .75.75v.005a.75.75 0 0 1-.75.75h-.006a.75.75 0 0 1-.75-.75v-.005ZM14.25 16.494a.75.75 0 0 0-.75.75v.006c0 .414.335.75.75.75h.005a.75.75 0 0 0 .75-.75v-.006a.75.75 0 0 0-.75-.75h-.005ZM15.75 14.995a.75.75 0 0 1 .75-.75h.005a.75.75 0 0 1 .75.75v.006a.75.75 0 0 1-.75.75H16.5a.75.75 0 0 1-.75-.75v-.006ZM13.498 12.743a.75.75 0 0 1 .75-.75h2.25a.75.75 0 1 1 0 1.5h-2.25a.75.75 0 0 1-.75-.75ZM6.748 14.993a.75.75 0 0 1 .75-.75h4.5a.75.75 0 0 1 0 1.5h-4.5a.75.75 0 0 1-.75-.75Z"
+								/>
+								<path
+									fill-rule="evenodd"
+									d="M18 2.993a.75.75 0 0 0-1.5 0v1.5h-9V2.994a.75.75 0 1 0-1.5 0v1.497h-.752a3 3 0 0 0-3 3v11.252a3 3 0 0 0 3 3h13.5a3 3 0 0 0 3-3V7.492a3 3 0 0 0-3-3H18V2.993ZM3.748 18.743v-7.5a1.5 1.5 0 0 1 1.5-1.5h13.5a1.5 1.5 0 0 1 1.5 1.5v7.5a1.5 1.5 0 0 1-1.5 1.5h-13.5a1.5 1.5 0 0 1-1.5-1.5Z"
+									clip-rule="evenodd"
+								/>
+							</svg>
+						</a>
+					{/if}
 					<button
 						onclick={complete_selected_subjects}
 						title="Marcar como completadas"
